99fc987f90d27c38c1ba4b99409767bce62ddee9
Wali Muhammad <luckywali0986@gmail.com>
2023-01-14 01:22:53 +0500

    fix the bug of values getting concatenates with eact other and no operation sign shoowing while calculating values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
 import './style/style.scss';
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/calculator';
 import './components/logic/calculate';
 import './components/logic/operate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
